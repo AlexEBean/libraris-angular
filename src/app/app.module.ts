6a08b8f7970d2133eb07ebb88547ea1a16884e86
Alex <alexbean95@gmail.com>
2021-07-26 16:33:42 -0600

    generated necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AccountComponent,
+    AdminComponent,
+    CatalogComponent,
+    LoginComponent,
+    NavComponent,
+    RegisterComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
